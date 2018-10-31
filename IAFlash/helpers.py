e280c2115f88e9e949ff1865e4439fbe23ddbf4a
0-tree <arthur.roullier@yahoo.com>
2018-10-31 13:45:06 +0100

    data: ok created easy-to-manipulate dataset

--- IAFlash/helpers.py
+++ IAFlash/helpers.py
@@ -13,55 +13,44 @@ import scipy.io # used to import .mat files with scipy.io.loadmat (see [here](ht
 
 class DataHandler:
     
-    def __init__(self, pathToDataDir, whichList):
+    def __init__(self, pathToDataDir):
         '''
         Inputs
         ------
         pathToDataDir : str
             path to the directory containing Images/ Annotations/ Lists/ etc.
-        whichList : str, in ('all','train','test')
-            which list of files should be used.    
+
         Notes
         -----
         - this class assumes no change is made in the structure of the
-          directory downloaded from http://vision.stanford.edu/aditya86/ImageNetDogs/
-          nor in files names
-        - except: Annotation/ <- Annotation*s*/
+          downloaded directory
         '''
 
-        self.whichList = whichList
         self.pathToDataDir = pathToDataDir   
-        self.pathToDataImageDir = os.path.join(pathToDataDir,'Images')
-        self.pathToDataAnnotationDir = os.path.join(pathToDataDir,'Annotations')
-        self.pathToDataListDir = os.path.join(pathToDataDir,'Lists')
-
-        for _p in (self.pathToDataDir,
-                   self.pathToDataImageDir,
-                   self.pathToDataAnnotationDir,
-                   self.pathToDataListDir):
+        self.pathToDataPatchDir = os.path.join(pathToDataDir, 'Patches') 
+        # str : directory where patches are saved
+        
+        for _p in (self.pathToDataDir,):
             if not os.path.isdir(_p):
                 raise ValueError('directory does not exist: {}'.format(_p))
 
-        self._loadLists()
-        self.n = len(self._fileList_file)
+        self._loadAnnos()
+        self.n = len(self._cars_annos_files)
         
 
-    def _loadLists(self):
+    def _loadAnnos(self):
         '''
         '''
-        if self.whichList == 'all':
-            _name = 'file'
-        elif self.whichList == 'train':
-            _name = 'train'
-        elif self.whichList == 'test':
-            _name = 'test'
-        else:
-            raise ValueError('unkown whichList: {}'.format(self.whichList))
-            
-        _list = scipy.io.loadmat(os.path.join(self.pathToDataListDir,_name+'_list.mat'))
-        self._fileList_file = [x[0][0] for x in _list['file_list']]
-        self._fileList_annotation = [x[0][0] for x in _list['annotation_list']]
-        self._fileList_label = [x[0] for x in _list['labels']]
+        cars_annos = scipy.io.loadmat(self.pathToDataDir+'cars_annos.mat')
+
+        self._cars_annos_bbox = [(int(x[1][0,0]),
+                                  int(x[2][0,0]),
+                                  int(x[3][0,0]),
+                                  int(x[4][0,0])) for x in cars_annos['annotations'][0,:]]
+        self._cars_annos_files = [x[0][0] for x in cars_annos['annotations'][0,:]]
+        self._cars_annos_labels = [x[5][0,0]-1 for x in cars_annos['annotations'][0,:]] # NB: change to 0-based index
+        self._cars_annos_isTest = [1 == x[6][0,0] for x in cars_annos['annotations'][0,:]]
+        self._cars_annos_classnames = [x[0] for x in cars_annos['class_names'][0,:]]
             
     
     
@@ -69,39 +58,36 @@ class DataHandler:
                 
 class ImageWorker:
     
-    def __init__(self, dataHandler, indexInList):
+    def __init__(self, dataHandler, index):
         '''
         Inputs
         ------
         dataHandler : DataHandler
             DataHandler for the session.
-        indexInList : int
+        index : int
             file index in the file list.
         ''' 
 
-        _fileName = dataHandler._fileList_file[indexInList]
-        _annotationName = dataHandler._fileList_annotation[indexInList]
-        self.label = dataHandler._fileList_label[indexInList]
+        self._dh = dataHandler
+        self._fileName = self._dh._cars_annos_files[index] # contains an extra 'ca_ims'
+        self._shortFileName = os.path.split(self._fileName)[-1]
         
-        self.pathToImage = os.path.join(dataHandler.pathToDataImageDir, _fileName)
-        self.pathToAnnotation = os.path.join(dataHandler.pathToDataAnnotationDir, _annotationName)
-
-        for _p in (self.pathToImage,
-                   self.pathToAnnotation):
+        self.label = self._dh._cars_annos_labels[index]
+        self.labelName = self._dh._cars_annos_classnames[self.label]
+        self.bbox = self._dh._cars_annos_bbox[index]
+        self.isTest = self._dh._cars_annos_isTest[index]
+        
+        self.pathToImage = os.path.join(self._dh.pathToDataDir, self._fileName)
+        for _p in (self.pathToImage,):
             if not os.path.exists(_p):
                 raise ValueError('file does not exist: {}'.format(_p))
-                
-        # get the label from .pathToImage (can also use the lists.mat...)
-        _dir = os.path.normpath(self.pathToImage).split(os.sep)[-2]
-        self.labelName = re.findall('-(.*)',_dir)[0]
-        
-        # hard code
-        self.bndboxMarker = './/bndbox'
         
         # updated later
+        self.edge = None
+        self.bw = None
+        self.resampleFilter = None
         self.image = None
-        self.annotation = None
-        self.patches = None
+        self.patch = None
         # list of PIL.Images - list of patches, using the annotations, taking the smallest square containing it, and resizing.
         
         
@@ -110,14 +96,8 @@ class ImageWorker:
         '''
         self.image = Image.open(self.pathToImage)
         
-        
-    def loadAnnotation(self):
-        '''
-        '''
-        self.annotation = xml.etree.ElementTree.parse(self.pathToAnnotation)
-        
-        
-    def buildPatches(self, edge=256, resampleFilter=Image.BICUBIC):
+
+    def buildPatch(self, edge=256, bw=True, resampleFilter=Image.BICUBIC):
         '''
         crops images to centered squares.
         
@@ -125,33 +105,71 @@ class ImageWorker:
         ------
         edge : int, default 256
             size in pixel of the edge of the *square* patch.
+        bw : bool
+            whether to turn the image black&white.
         resampleFilter : _
             see PIL.Image.resize().
         '''
+        self.edge = edge
+        self.bw = bw
+        self.resampleFilter = resampleFilter
+        
         if self.image is None: self.loadImage()
-        if self.annotation is None: self.loadAnnotation()
-            
-        self.patches = []
-        allbndbox = self.annotation.findall(self.bndboxMarker)
-        for bndbox in allbndbox:
-            
-            # get the box
-            [xmin,ymin,xmax,ymax] = [int(e.text) for e in bndbox.getchildren()]
-            
-            # change it to square with corresponding center
-            xc,yc = int((xmax+xmin)/2),int((ymax+ymin)/2)
-            halfedge = int(max((xmax-xmin)/2,(ymax-ymin)/2))
-            
-            box_square_centered = (xc-halfedge,
-                                   yc-halfedge,
-                                   xc+halfedge,
-                                   yc+halfedge)
-            im_crop = self.image.crop(box_square_centered)
-            im_patch = im_crop.resize((edge,edge),resampleFilter)
-            
-            self.patches.append(im_patch)
+        
+        _im = self.image.copy()
+        
+        # convert to black&white
+        if bw: _im = _im.convert('L')
+        
+        # change it to square with corresponding center
+        [xmin,ymin,xmax,ymax] = self.bbox
+    
+        xc,yc = int((xmax+xmin)/2),int((ymax+ymin)/2)
+        halfedge = int(max((xmax-xmin)/2,(ymax-ymin)/2))
+    
+        box_square_centered = (xc-halfedge,
+                               yc-halfedge,
+                               xc+halfedge,
+                               yc+halfedge)
+        _im = _im.crop(box_square_centered)
+        _im = _im.resize((edge,edge))
                 
+        # return
+        self.patch = _im
+        
+ 
     
-    
+    def savePatch(self,method='oneDirPerClass'):
+        '''
+        save the patch to disk
+        
+        Inputs
+        ------
+        method : str in ('oneDirPerClass',)
+            - 'oneDirPerClass' saves the patch to a directory of the form
+              root/macroparam/isTest/labelName/file.png
+        '''
+        if method == 'oneDirPerClass':
+            
+            macroparam = ''
+            for k in ('edge','bw','resampleFilter'):
+                macroparam += k+str(getattr(self,k))
+            if self.isTest:
+                isTest = 'Test'
+            else:
+                isTest = 'Train'
+            labelName = self.labelName
+            
+            p = os.path.join(self._dh.pathToDataPatchDir,
+                             macroparam,
+                             isTest,
+                             labelName)
+            if not os.path.isdir(p):
+                os.makedirs(p)
+            self.patch.save(os.path.join(p, self._shortFileName))
+            
+        else:
+            raise NotImplementedError('method not implemented: {}'.format(method))
+        
 #%% END
       
